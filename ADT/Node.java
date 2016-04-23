195bfbae4527184e9eb44a48054414f820afa1b9
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-04-23 07:22:34 +0530

    -m

---
+++ ADT/Node.java
@@ -0,0 +1,24 @@
+package ADT;
+
+public class Node<K>{
+	
+	private K data;
+	private Node next;
+
+	public Node(K data,Node next){
+		this.data = data;
+		this.next = next;
+	}
+
+	public K getData(){
+		return this.data;
+	}
+
+	public Node getNext(){
+		return this.next;
+	}
+
+	public void setNext(Node nextNode){
+		this.next = nextNode;
+	}
+}
