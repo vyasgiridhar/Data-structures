6c4dae5882f8e1f11e424795909ce5b9cc4c4f28
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-05-01 02:37:51 +0530

    Adding Graphs

--- Hash Table/HashTable.java
+++ Hash Table/HashTable.java
@@ -14,4 +14,68 @@ public class HashTable<D,V> {
 
         }
 
-        public D 
+        public D getKey(){
+
+        	return this.key;
+        
+        }
+
+        publiv V getValue(){
+
+        	return this.value;
+        
+        }
+
+        public void updateValue(V value){
+
+        	this.value = value;
+        
+        }
+
+        public boolean matchBoth(D key, V value){
+
+        	return(this.key==key && this.value == value);
+        
+        }
+
+        public boolean matchKey(K key){
+            
+            if(key.getClass()==this.key.getClass()){
+            
+                if(key instanceof Double || key instanceof Float){
+                    return key.toString().equals(this.key.toString());
+                }
+                else{
+                    return key==this.key;
+                }
+            }
+            return false;
+        }      
+
+        @Override
+        public String toString(){
+        	String returnStr = "";
+
+        	if(this.key instanceof String){
+        		returnStr += ("\""+ this.key + "\"");
+        	}
+        	else if(this.key instanceof Character){
+        		returnStr +=("'" + this.key + "':");
+        	}
+            else{
+                returnStr += (this.key+": ");
+            }
+            if(this.value instanceof String){
+                returnStr += ("\""+this.value+"\"");
+            }
+            else if(this.value instanceof Character){
+                returnStr += ("'"+this.value+"'");
+            }
+            else{
+                returnStr += this.value;
+            }            
+            return returnStr;
+        }
+
+    }
+}
